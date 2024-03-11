d55dd7566d34fae10b48caf2162478b4a6d0a4c2
Yudagroper <tanaka.jobmail.yudai@gmail.com>
2024-03-11 17:27:44 +0900

    new.html.erbの編集

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -2,8 +2,14 @@ class MarketsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    #@markets = Market.all
+    @markets = Market.all
   end
 
+  def new
+    @market = Market.new
+  end
+
+  def create
+  end
 
 end
