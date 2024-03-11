3c12bf3094592fa49083515626f1762a0e6a181c
Yudagroper <tanaka.jobmail.yudai@gmail.com>
2024-03-11 18:34:44 +0900

    画像投稿機能の実装

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,3 +1,29 @@
 class Market < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  # テーブルとのアソシエーション
   belongs_to :user
+
+
+  # アクティブハッシュとのアソシエーション
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :item_status
+  belongs_to :shipping_fee
+  belongs_to :shipping_day
+
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :user
+    validates :image
+    validates :goods
+    validates :explan 
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
 end
