d3824c341bf9e3474510d8a455de6386d17c7ddc
Yudagroper <tanaka.jobmail.yudai@gmail.com>
2024-03-12 16:34:15 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: 'markets#index'
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :markets, only: :index
+  resources :markets, only: [:index, :new, :create]
 end
